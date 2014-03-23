e319d5c7a91c70b7bcb976fb310a41fc090763e6
Carlito <post@carlito.me>
2014-03-23 12:58:46 +0100

    Added readme.md, Converted tabs to spaces.

--- log.rb
+++ log.rb
@@ -4,34 +4,34 @@ args = ARGV;
 
 def log(args)
 
-	logsLocation  = Dir.home + '/Dropbox/worklog/'
-	fileExtension = 'txt'
-	separator     = ' - ' 
+  logsLocation  = Dir.home + '/Dropbox/worklog/'
+  fileExtension = 'txt'
+  separator     = ' - ' 
 
-	dateFormat = '%Y-%m-%d'
-	timeFormat = '%H:%M:%S'
+  dateFormat = '%Y-%m-%d'
+  timeFormat = '%H:%M:%S'
 
-	message = args[0]
+  message = args[0]
 
-	currentDate = Time.now.strftime(dateFormat)
-	currentTime = Time.now.strftime(timeFormat)
+  currentDate = Time.now.strftime(dateFormat)
+  currentTime = Time.now.strftime(timeFormat)
 
-	filePath = logsLocation + currentDate + '.' + fileExtension
+  filePath = logsLocation + currentDate + '.' + fileExtension
 
-    newEntry = currentTime + separator + message	
-	newFileContent = File.exists?(filePath) ? File.read(filePath)+newEntry : newEntry;
+  newEntry = currentTime + separator + message
+  newFileContent = File.exists?(filePath) ? File.read(filePath)+newEntry : newEntry;
 
-	File.open(filePath,'w+') do |file|
+  File.open(filePath,'w+') do |file|
 
-	  	file.puts newFileContent
-		
-		successMsgLine1 = 'Log written in ' + filePath
-		successMsgLine2 = 'Your log: ' + newEntry
-		
-		puts successMsgLine1.colorize(32)
-		puts successMsgLine2.colorize(90)
-	
-	end
+    file.puts newFileContent
+    
+    successMsgLine1 = 'Log written in ' + filePath
+    successMsgLine2 = 'Your log: ' + newEntry
+    
+    puts successMsgLine1.colorize(32)
+    puts successMsgLine2.colorize(90)
+  
+  end
 
 end
 
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Log.rb
 
 A little script to log something.
+
+Usage:
+
+> ruby log.rb 'Message to log.'
